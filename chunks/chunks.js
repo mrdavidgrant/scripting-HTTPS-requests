cf62b24e825725e100eab28f8737970b079bb079
David Grant <mrdavidgrant@outlook.com>
2017-09-11 15:52:05 +0000

    step one complete

---
+++ chunks/chunks.js
@@ -0,0 +1,20 @@
+function getAndPrintHTMLChunks () {
+  
+    var requestOptions = {
+      host: 'sytantris.github.io',
+      path: '/http-examples/step1.html'
+    };
+  
+    /* Add your code here */
+    var https = require('https')
+    var results = ""
+    https.get(requestOptions, function(response){
+      response.setEncoding = 'utf8'
+      response.on('data', function(data) {
+        results += data
+        console.log(results)
+      })
+    })
+  }
+
+  getAndPrintHTMLChunks()
